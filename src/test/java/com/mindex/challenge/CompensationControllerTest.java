78878131fc111ae41c8b5bbd1abcd3dcafab9bb5
mindex <dev@mindex.com>
2025-04-04 03:05:29 -0400

    Hesitant final product

--- src/test/java/com/mindex/challenge/CompensationControllerTest.java
+++ src/test/java/com/mindex/challenge/CompensationControllerTest.java
@@ -6,6 +6,7 @@ import com.mindex.challenge.service.impl.CompensationServiceImplTest;
 import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.CompensationService;
+import org.apache.coyote.Response;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -38,17 +39,29 @@ public class CompensationControllerTest {
     @Mock
     private CompensationService mockCompensationService;
 
+    /**
+     * Tests the create method for succeeding in the controller
+     * This test checks for
+     * - a CREATED Http status code
+     * - the created compensation matches the compensation passed to the
+     * service by the controller
+     */
     @Test
     public void testCreateSuccessful() {
         Compensation compensation = new Compensation();
         when(mockCompensationService.create(compensation)).thenReturn(compensation);
         ResponseEntity<Compensation> response = compensationController.create(compensation);
-        //check that employee was created successfully
+        //check that Compensation was created successfully
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
         //check that the Compensation objects match
         assertCompensationEquivalence(compensation, response.getBody());
     }
 
+    /**
+     * Tests the create method for failing in the controller
+     * This test checks for
+     * - a BAD_REQUEST Http status code
+     */
     @Test
     public void testCreateFailed() {
         Compensation compensation = new Compensation();
@@ -57,18 +70,90 @@ public class CompensationControllerTest {
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
+    /**
+     * Tests the read method for succeeding in the controller
+     * This test checks for
+     * - an OK Http status code
+     * - the read compensation matches the compensation with the
+     * corresponding compensationID
+     */
     @Test
     public void testReadSuccessful() {
         Compensation compensation = new Compensation(
                 "5678",
                 "1234",
-                -1,
+                100000,
                 LocalDate.of(2000, 1, 1)
         );
         when(mockCompensationService.read(compensation.getCompensationID())).thenReturn(compensation);
         ResponseEntity<Compensation> response = compensationController.read(compensation.getCompensationID());
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertCompensationEquivalence(compensation, response.getBody());
+    }
+
+    /**
+     * Tests the read method for failing in the controller
+     * This test checks for
+     * - a NOT_FOUND http status code
+     */
+    @Test
+    public void testReadFail() {
+        Compensation compensation = new Compensation(
+                "5678",
+                "1234",
+                100000,
+                LocalDate.of(2000, 1, 1)
+        );
+        when(mockCompensationService.read(compensation.getCompensationID())).thenThrow(new RuntimeException());
+        ResponseEntity<Compensation> response = compensationController.read(compensation.getCompensationID());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    /**
+     * Tests the update method for succeeding in the controller
+     * This test checks for
+     * - an OK Http status code
+     * - the returned compensation matches the compensation that was passed
+     * to the update
+     */
+    @Test
+    public void testUpdateSuccessful() {
+        // assume this is the new compensation after the update
+        Compensation compensation = new Compensation(
+                "5678",
+                "1234",
+                100000,
+                LocalDate.of(2000, 1, 1)
+        );
+        when(mockCompensationService.update(compensation)).thenReturn(compensation);
+        ResponseEntity<Compensation> response = compensationController.update(
+                        compensation.getCompensationID(),
+                        compensation
+            );
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertCompensationEquivalence(compensation, response.getBody());
+    }
+
+    /**
+     * Tests the update method for failing in the controller
+     * This test checks for
+     * - a BAD_REQUEST Http status code
+     */
+    @Test
+    public void testUpdateFail() {
+        // assume this is the new compensation after the update
+        Compensation compensation = new Compensation(
+                "5678",
+                "1234",
+                100000,
+                LocalDate.of(2000, 1, 1)
+        );
+        when(mockCompensationService.update(compensation)).thenReturn(null);
+        ResponseEntity<Compensation> response = compensationController.update(
+                compensation.getCompensationID(),
+                compensation
+        );
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
     /**
